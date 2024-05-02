e45159c4560b767069556e9f1c60183cc08d90bd
Luc Perkins <lucperkins@gmail.com>
2024-05-02 16:31:37 -0300

    Use tsup to build

---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "tsup";
+import { name } from "./package.json";
+
+export default defineConfig({
+  name,
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  target: "node20",
+  bundle: true,
+  splitting: false,
+  clean: true,
+  dts: {
+    resolve: true,
+  },
+});
