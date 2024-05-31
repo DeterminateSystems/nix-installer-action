a2473ab552bbd5ffa381e52c0f965e554aa0bb6d
Graham Christensen <graham@grahamc.com>
2024-05-31 12:30:37 -0400

    Srv update (#97)
    * Update to srv
    
    * Hi, footgun
    
    * Make sure we don't toString promises, update deps
    
    * Update
    
    * Pass - as the default diagnostic endpoint
    
    * Specify the diagnostic suffix
    
    * Update deps
    
    * SRV update

--- src/index.ts
+++ src/index.ts
@@ -80,6 +80,7 @@ class NixInstallerAction extends DetSysAction {
       fetchStyle: "nix-style",
       legacySourcePrefix: "nix-installer",
       requireNix: "ignore",
+      diagnosticsSuffix: "diagnostic",
     });
 
     this.platform = platform.getNixPlatform(platform.getArchOs());
@@ -402,7 +403,7 @@ class NixInstallerAction extends DetSysAction {
     }
 
     executionEnv.NIX_INSTALLER_DIAGNOSTIC_ENDPOINT =
-      this.getDiagnosticsUrl()?.toString() ?? "";
+      (await this.getDiagnosticsUrl())?.toString() ?? "";
 
     // TODO: Error if the user uses these on not-MacOS
     if (this.macEncrypt !== null) {
